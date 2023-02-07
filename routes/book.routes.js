6c44036467bf11cdc3173d16beccf79c45a49af3
Julian Rivera <julianarevircoins@gmail.com>
2023-02-07 14:47:50 -0500

    addbook

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -11,7 +11,7 @@ router.get("/", (req, res, next) => {
       console.log("Retrieved books from DB:", allTheBooksFromDB);
 
       // we call the render method after we obtain the books data from the database -> allTheBooksFromDB
-      res.render("books/book-list.hbs", { books: allTheBooksFromDB }); // pass `allTheBooksFromDB` to the view (as a variable books to be used in the HBS)
+      res.render("books/book-list.hbs", { books: allTheBooksFromDB.reverse() }); // pass `allTheBooksFromDB` to the view (as a variable books to be used in the HBS)
     })
     .catch((error) => {
       console.log("Error while getting the books from the DB: ", error);
@@ -21,19 +21,32 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.get("/:bookId", (req, res, next) => {
-  const { bookId } = req.params;
-  Book.findById(bookId)
-    .then((foundBook) => {
-      console.log(foundBook);
-      res.render("books/book-details.hbs", foundBook);
+router.get("/create", (req, res, next) => {
+  res.render("books/new-book.hbs");
+});
+router.post("/create", (req, res, next) => {
+  const { title, description, author, rating } = req.body;
+
+  Book.create({ title, description, author, rating })
+    .then((newBook) => {
+      console.log("Book Created:", newBook);
+      res.redirect("/books");
     })
     .catch((err) => {
       console.log(err);
     });
 });
-router.get("/create", (req, res, next) => {
-  res.render("books/new-book.hbs");
+
+router.get("/:bookId", (req, res, next) => {
+  const { bookId } = req.params;
+  Book.findById(bookId)
+    .then((foundBook) => {
+      console.log("Found Book:", foundBook);
+      res.render("books/book-details.hbs", foundBook);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
